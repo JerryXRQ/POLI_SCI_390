c9d8d53fa4f711bce83739ebbc7a68b645ee0a1c
JerryXRQ <55816134+JerryXRQ@users.noreply.github.com>
2022-10-05 21:27:42 -0500

    Add Graph Code

--- Data/VisBlog1/script.R
+++ Data/VisBlog1/script.R
@@ -1,11 +1,16 @@
 library(tidyverse)
 install.packages("tidyverse", dependencies=T)
 
-data <- read.csv("VisBlog1.csv")
+data <- na.omit(read.csv("VisBlog1.csv"))
+
 
 data %>%
-  group_by(year)%>%
-  summarize(sum_cases=sum(cases),na.rm=T)%>%
-  ggplot(aes(x=year,y=sum_cases))+
-  scale_y_continuous(limits=c(0,20000000)) +
+  group_by(month,year)%>%
+  summarize(sum_cases=sum(deaths),na.rm=T)%>%
+  ggplot(aes(y=month,x=sum_cases, color=as_factor(year)))+
+  scale_y_continuous(breaks=c(1,2,3,4,5,6,7,8,9,10,11,12))+
+  labs(x="Number of Confirmed Cases Recorded", y = "Month", 
+       color="Year",
+       title="Europe COVID Cases by Month",
+       subtitle ="Plot of recorded COVID cases in the Dataset from European Centre for Disease Prevention and Control")+
   geom_point()
