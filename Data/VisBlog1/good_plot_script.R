f911d53688d6f27a829a20ed0bbe88eebb161985
JerryXRQ <55816134+JerryXRQ@users.noreply.github.com>
2022-10-10 13:50:40 -0500

    Update Script

--- Data/VisBlog1/good_plot_script.R
+++ Data/VisBlog1/good_plot_script.R
@@ -4,5 +4,8 @@ value <- abs(rnorm(8,0))
 mock <- data.frame(rate,condition,value)
 
 mock %>% ggplot(aes(fill=condition, y=value, x=country)) + 
+  ylim(0,2)+
+  labs(x = "Country", y = "Growth of e-commerce as a share of total retail (%)") +
+  scale_fill_discrete(name = "Year", labels = c("2020 average", "2015-2019 average")) +
   geom_bar(position="dodge", stat="identity")
 
