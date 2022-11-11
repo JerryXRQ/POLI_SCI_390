94ebea902c529da449af2a7052bc2081d1c4991e
JerryXRQ <55816134+JerryXRQ@users.noreply.github.com>
2022-11-10 21:19:12 -0600

    Update based on feedback

--- Data/ShortFormBlog2/script.R
+++ Data/ShortFormBlog2/script.R
@@ -1,13 +1,13 @@
 #install.packages("rlang") 
 # install devtools, then use that to install the vdem data
-#install.packages("devtools")
+#install.packages("ggdist")
 devtools::install_github("vdeminstitute/vdemdata")
 
 library(devtools)
 library(vdemdata)
 
 packages <- c("tidyverse", "reshape2", "fauxnaif", "gganimate", "ggthemes",
-              "stringr", "gridExtra", "gifski", "png", "ggrepel", "scales",
+              "stringr", "gridExtra", "gifski", "png", "ggrepel", "scales", "ggdist",
               "lubridate", "paletteer", "GGally", "systemfonts", "extrafont", "colorspace")
 lapply(packages, require, character.only = TRUE)
 loadfonts(device = "all")
@@ -18,13 +18,16 @@ vdem %>%
   filter(year == 2020) %>% 
   ggplot(aes(y = v2x_execorr, x = v2mecenefm_osp)) + 
   facet_wrap(~ e_regionpol_6C, scales="free",labeller = labeller(e_regionpol_6C = new_labels)) +
-  geom_text(aes(label = country_name),size=2.0,check_overlap = T, nudge_y = -0.02)+
-  labs(x = "Government Censorship effort—Media", y = "Executive corruption index ", 
+  geom_text(aes(label = country_name),size=3.0,check_overlap = T, nudge_y = -0.02)+
+  labs(x = "Government Censorship Effort—Media", y = "Executive Corruption Index ", 
        title = "Relationship between Government Censorship of Media and Executive Corruption, 2020") +
-  theme_stata()+theme(axis.title.x = element_text(size = 15),axis.title.y = element_text(size = 15),
-                      plot.title = element_text(size = 18), axis.text.y = element_text(size = 12),
-                      axis.text.x = element_text(size = 12))+
+  theme_stata()+theme(axis.title.x = element_text(size = 18,margin = margin(t = 8)),axis.title.y = element_text(size = 18,margin = margin(r = 8)),
+                      plot.title = element_text(size = 20), axis.text.y = element_text(size = 13),
+                      axis.text.x = element_text(size = 13),
+                      strip.text.x = element_text(size = 15),
+                      panel.spacing = unit(1.2, "lines"))+
   geom_point()->plot1
+plot1
 ggsave(plot=plot1,filename="Plot1.png",dpi = 1000)
 
 vdem %>% 
@@ -42,9 +45,10 @@ vdem %>%
   theme(legend.position = "none")+
   geom_bar(stat='identity')+
   geom_errorbar(aes(ymin = lower_bound, ymax = upper_bound), width = 0.3,size=0.5)+ 
-  theme(axis.title.x = element_text(size = 15),axis.title.y = element_text(size = 15),
-        plot.title = element_text(size = 18), axis.text.y = element_text(size = 12),
-        axis.text.x = element_text(size = 12))
+  theme(axis.title.x = element_text(size = 20, margin = margin(t = 8),),
+        plot.title = element_text(size = 24), axis.text.y = element_text(size = 14),
+        axis.text.x = element_text(size = 14),
+        axis.title.y = element_text(margin = margin(r = 8),size = 20))
   
 vdem %>% filter(year>2000)%>% 
   group_by(e_regionpol_6C,year) %>%
@@ -55,5 +59,27 @@ vdem %>% filter(year>2000)%>%
        title = "Relationship between Government Censorship of Media and Executive Corruption from 2000 to 2020") +
   geom_line(aes(y = mean_corr, x = year,color='Mean Corruption Index'),size=0.8)+
   geom_line(aes(y = mean_cens, x = year,color='Mean Censorship Index'),size=0.8)+ 
-  theme_stata() + theme(axis.title.x = element_text(size = 15),axis.title.y = element_text(size = 15),
-                        plot.title = element_text(size = 18))
+  theme_stata() + theme(axis.title.x = element_text(size = 18,margin = margin(t = 8)),axis.title.y = element_text(size = 18,margin = margin(r = 8)),
+                        plot.title = element_text(size = 24),legend.text = element_text(size = 13),strip.text.x = element_text(size = 15),
+                        panel.spacing = unit(1.2, "lines"),axis.text.x = element_text(size = 13),axis.text.y = element_text(size = 13),)
+
+vdem %>% 
+  filter(year == 2020) %>% 
+  mutate(ranges=cut(v2mecenefm,seq(-4, 4, 1),labels=c("-4 to -3","-3 to -2","-2 to -1","-1 to 0","-1 to 0","0 to 1","1 to 2","2 to 3"))) %>%
+  group_by(ranges)%>%
+  mutate(average=mean(v2x_execorr)) %>%
+  ggplot(aes(x=v2x_execorr,fill=ranges))+
+  labs(x = "Executive Corruption Index", y = "Density in Corresponding Media Censorship Level",
+       title = "Density Plot of Executive Corruption Index based on Level of Government Censorship of Media, 2020") +
+  theme_stata()+
+  geom_density() +
+  geom_vline(aes(xintercept=average),
+           color="blue", linetype="dashed", size=1) +
+  facet_wrap(~ ranges, scales="free")+
+  theme(axis.title.x = element_text(size = 18,margin = margin(t = 8)),axis.title.y = element_text(size = 18,margin = margin(r = 8)),
+        plot.title = element_text(size = 24), axis.text.y = element_text(size = 14),strip.text.x = element_text(size = 15),
+       axis.text.x = element_text(size = 14),legend.position = "none") ->plot2
+plot2
+ggsave(plot=plot2,filename="Plot4.jpg",width = 15, height = 8)
+
+
