0394f6a3a914bcbe6f55f6b07946afe8400e6fe6
JerryXRQ <55816134+JerryXRQ@users.noreply.github.com>
2022-12-09 22:43:07 -0600

    Viz Blog 1 Feedback Included

--- Data/VisBlog1/good_plot_script.R
+++ Data/VisBlog1/good_plot_script.R
@@ -5,7 +5,13 @@ mock <- data.frame(rate,condition,value)
 
 mock %>% ggplot(aes(fill=condition, y=value, x=country)) + 
   ylim(0,2)+
-  labs(x = "Country", y = "Growth of e-commerce as a share of total retail (%)") +
+  labs(x = "Country", y = "Growth of e-commerce as a share of total retail (%)",title="Growth of E-Commerce") +
   scale_fill_discrete(name = "Year", labels = c("2020 average", "2015-2019 average")) +
+  theme(axis.title.x = element_text(size = 14, margin = margin(t = 8)),
+        plot.title = element_text(size = 16), axis.text.y = element_text(size = 10, angle=0),
+        axis.text.x = element_text(size = 14),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        axis.title.y = element_text(margin = margin(r = 8),size = 14))+
   geom_bar(position="dodge", stat="identity")
 
